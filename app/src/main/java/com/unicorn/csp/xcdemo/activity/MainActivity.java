8514000ed087afe1c4baf707c09ec369c216d87f
renjiajian <renjiajian@withub.net.cn>
2015-10-17 15:51:25 +0800

    just some change

--- app/src/main/java/com/unicorn/csp/xcdemo/activity/MainActivity.java
+++ app/src/main/java/com/unicorn/csp/xcdemo/activity/MainActivity.java
@@ -3,6 +3,10 @@ package com.unicorn.csp.xcdemo.activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.unicorn.csp.xcdemo.R;
 import com.unicorn.csp.xcdemo.activity.base.ToolbarActivity;
 import com.unicorn.csp.xcdemo.adaper.pager.ViewPagerAdapter;
@@ -19,6 +23,8 @@ public class MainActivity extends ToolbarActivity {
     @Bind(R.id.viewpager)
     ViewPager viewPager;
 
+    Drawer drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -32,6 +38,40 @@ public class MainActivity extends ToolbarActivity {
 
         viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
         tabs.setViewPager(viewPager);
+        initDrawer();
+    }
+
+    private void initDrawer() {
+
+        drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withTranslucentStatusBar(false)
+                .withToolbar(getToolbar())
+                .withActionBarDrawerToggleAnimated(true)
+                .withHeader(R.layout.drawer_header)
+                .withHeaderDivider(false)
+                .addDrawerItems(
+                        getFirstDrawerItem(),
+                        getOtherDrawerItem(),
+                        getOtherDrawerItem(),
+                        getOtherDrawerItem()
+                )
+                .build();
+    }
+
+    private PrimaryDrawerItem getFirstDrawerItem() {
+
+        return new PrimaryDrawerItem().
+                withName("工作清单")
+                .withIcon(GoogleMaterial.Icon.gmd_assignment)
+                .withBadge("10");
+    }
+
+    private PrimaryDrawerItem getOtherDrawerItem() {
+
+        return new PrimaryDrawerItem().
+                withName("待定")
+                .withIcon(GoogleMaterial.Icon.gmd_assignment_ind);
     }
 
 }
