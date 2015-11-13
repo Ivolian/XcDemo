971f140367467f2cb8430598f1ae38930c5a3367
renjiajian <renjiajian@withub.net.cn>
2015-11-13 14:55:57 +0800

    some change

--- app/src/main/java/com/unicorn/csp/xcdemo/activity/shared/LoginActivity.java
+++ app/src/main/java/com/unicorn/csp/xcdemo/activity/shared/LoginActivity.java
@@ -12,11 +12,13 @@ import com.android.volley.toolbox.StringRequest;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.unicorn.csp.xcdemo.R;
 import com.unicorn.csp.xcdemo.activity.base.ToolbarActivity;
+import com.unicorn.csp.xcdemo.activity.chief.TodoActivity;
 import com.unicorn.csp.xcdemo.activity.technician.WorkOrderActivity;
 import com.unicorn.csp.xcdemo.component.TinyDB;
 import com.unicorn.csp.xcdemo.utils.ConfigUtils;
 import com.unicorn.csp.xcdemo.utils.ToastUtils;
 import com.unicorn.csp.xcdemo.volley.SimpleVolley;
+import com.unicorn.csp.xcdemo.volley.VolleyErrorHelper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -48,16 +50,22 @@ public class LoginActivity extends ToolbarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initToolbar("登录", false);
+
+        etAccount.setText("js1");
+        etPassword.setText("123456");
     }
 
+    String role = "";
+
+    String shiroLoginFailure = null;
 
     // ================================== OnClick ==================================
 
     @OnClick(R.id.btn_login)
     public void onLoginBtnClick() {
-//        if (isUserInputValid()) {
+        if (isUserInputValid()) {
             login(showMask());
-//        }
+        }
     }
 
     private void login(final MaterialDialog mask) {
@@ -69,29 +77,40 @@ public class LoginActivity extends ToolbarActivity {
                             @Override
                             public void onResponse(String response) {
                                 mask.dismiss();
-                                startActivityAndFinish(WorkOrderActivity.class);
+                                if (shiroLoginFailure!=null){
+                                    ToastUtils.show("登录失败！");
+                                    return;
+                                }
+                                startActivityAndFinish(role.equals("Artificer") ? WorkOrderActivity.class : TodoActivity.class);
                             }
                         },
                         new Response.ErrorListener() {
                             @Override
                             public void onErrorResponse(VolleyError error) {
                                 mask.dismiss();
-                                startActivityAndFinish(WorkOrderActivity.class);
-//                                ToastUtils.show(VolleyErrorHelper.getErrorMessage(error));
+//                                startActivityAndFinish(WorkOrderActivity.class);
+                                ToastUtils.show(VolleyErrorHelper.getErrorMessage(error));
                             }
                         }
                 ) {
                     @Override
                     protected Map<String, String> getParams() throws AuthFailureError {
                         Map<String, String> map = new HashMap<>();
-                        map.put("username", "js1");
-                        map.put("password", "123456");
+                        map.put("username", etAccount.getText().toString().trim());
+                        map.put("password", etPassword.getText().toString().trim());
                         return map;
                     }
 
                     @Override
                     protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                        shiroLoginFailure = response.headers.get("shiroLoginFailure");
+                        if (shiroLoginFailure != null){
+                            return super.parseNetworkResponse(response);
+                        }
+
                         TinyDB.getInstance().putString(JSESSION_ID, response.headers.get(JSESSION_ID));
+                        role = response.headers.get("role");
+
                         return super.parseNetworkResponse(response);
                     }
                 }
