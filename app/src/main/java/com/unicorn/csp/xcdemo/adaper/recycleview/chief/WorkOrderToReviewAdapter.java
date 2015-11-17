22d0b17dd7b4598ac47c64b9bd50835418d1de8a
renjiajian <renjiajian@withub.net.cn>
2015-11-17 22:33:30 +0800

    some change

--- app/src/main/java/com/unicorn/csp/xcdemo/adaper/recycleview/chief/WorkOrderToReviewAdapter.java
+++ app/src/main/java/com/unicorn/csp/xcdemo/adaper/recycleview/chief/WorkOrderToReviewAdapter.java
@@ -3,6 +3,7 @@ package com.unicorn.csp.xcdemo.adaper.recycleview.chief;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import com.unicorn.csp.xcdemo.R;
 import com.unicorn.csp.xcdemo.activity.chief.ReviewActivity;
+import com.unicorn.csp.xcdemo.activity.technician.WorkOrderDetailActivity;
 import com.unicorn.csp.xcdemo.adaper.recycleview.technician.RefreshAdapter;
 import com.unicorn.csp.xcdemo.component.PaperButton;
 import com.unicorn.csp.xcdemo.model.WorkOrderInfo;
@@ -75,6 +77,15 @@ public class WorkOrderToReviewAdapter extends RecyclerView.Adapter<WorkOrderToRe
         ViewHolder(View view) {
             super(view);
             ButterKnife.bind(this, view);
+        }
+
+        @OnClick(R.id.cardview)
+        public void startWorkOrderDetailActivity(CardView cardView) {
+            Context context = cardView.getContext();
+            Intent intent = new Intent(context, WorkOrderDetailActivity.class);
+            intent.putExtra("workOrderProcessInfo", workOrderProcessInfoList.get(getAdapterPosition()));
+            context.startActivity(intent);
+            ((Activity) context).overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
         }
 
         @OnClick(R.id.btn_review)
