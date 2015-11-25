2024a0041f9307fa636a3f5fc1cbe63e807cd6f0
renjiajian <renjiajian@withub.net.cn>
2015-11-25 16:04:28 +0800

    Tab OK?

---
+++ app/src/main/java/com/unicorn/csp/xcdemo/component/WorkOrderCard.java
@@ -0,0 +1,188 @@
+package com.unicorn.csp.xcdemo.component;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
+import com.unicorn.csp.xcdemo.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+public class WorkOrderCard extends CardView {
+
+//    @Bind(R.id.labelview)
+//    LabelView labelView;
+//
+//    //
+//
+//    @Bind(R.id.tv_request_user_and_call_number)
+//    TextView tvRequestUserAndCallNumber;
+//
+//    @Bind(R.id.tv_request_time)
+//    TextView tvRequestTime;
+//
+//    @Bind(R.id.tv_building_and_address)
+//    TextView tvBuildingAndAddress;
+//
+//    @Bind(R.id.tv_type)
+//    TextView tvType;
+//
+//    @Bind(R.id.tv_equipment_and_fault_type)
+//    TextView tvEquipmentAndFaultType;
+//
+//    //
+//
+//    @Bind(R.id.tv_processing_time_limit)
+//    TextView tvProcessingTimeLimit;
+//
+//    @Bind(R.id.tv_issuer)
+//    TextView tvIssuer;
+//
+//    @Bind(R.id.tv_issue_time)
+//    TextView tvIssueTime;
+//
+//    @Bind(R.id.tv_distributor)
+//    TextView tvDistributor;
+//
+//    @Bind(R.id.tv_distribute_time)
+//    TextView tvDistributeTime;
+//
+//    //
+//    @Bind(R.id.tv_receiver)
+//    TextView tvReceiver;
+//
+//    @Bind(R.id.tv_receive_time)
+//    TextView tvReceiverTime;
+//
+//    @Bind(R.id.tv_arrive_time)
+//    TextView tvArriveTime;
+//
+//    @Bind(R.id.tv_hang_up_time)
+//    TextView tvHangUpTime;
+//
+//    @Bind(R.id.tv_complete_time)
+//    TextView tvCompleteTime;
+//
+//    //
+//
+//    @Bind(R.id.tv_confirm)
+//    TextView tvConfirm;
+//
+//    @Bind(R.id.tv_confirm_time)
+//    TextView tvConfirmTime;
+//
+//    @Bind(R.id.tv_pack)
+//    TextView tvPack;
+
+    @Bind(R.id.expand_layout)
+    ExpandableRelativeLayout expandableLayout;
+
+    //
+
+    public WorkOrderCard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    //
+
+    private void init() {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.work_order_card, this, true);
+        ButterKnife.bind(this);
+
+
+    }
+
+    public void collapse(){
+        expandableLayout.moveChild(6);
+    }
+
+    public void expand(){
+        expandableLayout.expand();
+    }
+
+
+
+//    public void setWorkOrderInfo(WorkOrderInfo workOrderInfo) {
+//        String requestUserAndCallNumber = "报修电话: " + workOrderInfo.getCallNumber() + " " + workOrderInfo.getRequestUser();
+//        tvRequestUserAndCallNumber.setText(requestUserAndCallNumber);
+//        String requestTime = "报修时间: " + new DateTime(workOrderInfo.getRequestTime()).toString("yyyy-MM-dd HH:mm:ss");
+//        tvRequestTime.setText(requestTime);
+//        String buildingAndAddress = "保修地点: " + workOrderInfo.getBuilding() + "(" + workOrderInfo.getAddress() + ")";
+//        tvBuildingAndAddress.setText(buildingAndAddress);
+//        String type = "维修类型: " + workOrderInfo.getType();
+//        tvType.setText(type);
+//        String equipmentAndFaultType = "维修内容: " + workOrderInfo.getEquipment() + "(" + workOrderInfo.getFaultType() + ")";
+//        tvEquipmentAndFaultType.setText(equipmentAndFaultType);
+//        String processingTimeLimit = "是否时限: " + workOrderInfo.getProcessingTimeLimit();
+//        tvProcessingTimeLimit.setText(processingTimeLimit);
+//
+//        //
+//
+//        String issuer = "受理人员: " + workOrderInfo.getIssuer();
+//        tvIssuer.setText(issuer);
+//        String issuerTime = "受理时间: " + new DateTime(workOrderInfo.getIssueTime()).toString("yyyy-MM-dd HH:mm:ss");
+//        tvIssueTime.setText(issuerTime);
+//        if (workOrderInfo.getIssuer() == null) {
+//            tvIssuer.setVisibility(View.GONE);
+//            tvIssueTime.setVisibility(View.GONE);
+//        }
+//        String distributor = "派单人员: " + workOrderInfo.getDistributor();
+//        tvDistributor.setText(distributor);
+//        String distributeTime = "派单时间: " + new DateTime(workOrderInfo.getDistributeTime()).toString("yyyy-MM-dd HH:mm:ss");
+//        tvDistributeTime.setText(distributeTime);
+//        if (workOrderInfo.getDistributor() == null) {
+//            tvDistributor.setVisibility(View.GONE);
+//            tvDistributeTime.setVisibility(View.GONE);
+//        }
+//        String receiver = "接单人员: " + workOrderInfo.getReceiver();
+//        tvReceiver.setText(receiver);
+//        String receiverTime = "接单时间: " + new DateTime(workOrderInfo.getReceiveTime()).toString("yyyy-MM-dd HH:mm:ss");
+//        tvReceiverTime.setText(receiverTime);
+//        if (workOrderInfo.getReceiver() == null) {
+//            tvReceiver.setVisibility(View.GONE);
+//            tvReceiverTime.setVisibility(View.GONE);
+//        }
+//        String arriveTime = "到达时间: " + new DateTime(workOrderInfo.getArriveTime()).toString("yyyy-MM-dd HH:mm:ss");
+//        tvArriveTime.setText(arriveTime);
+//        if (workOrderInfo.getArriveTime() == 0) {
+//            tvArriveTime.setVisibility(View.GONE);
+//        }
+//        String hangUpTime = "挂单时间: " + new DateTime(workOrderInfo.getHangUpTime()).toString("yyyy-MM-dd HH:mm:ss");
+//        tvHangUpTime.setText(hangUpTime);
+//        if (workOrderInfo.getHangUpTime() == 0) {
+//            tvHangUpTime.setVisibility(View.GONE);
+//        }
+//        String completeTime = "结单时间: " + new DateTime(workOrderInfo.getCompleteTime()).toString("yyyy-MM-dd HH:mm:ss");
+//        tvCompleteTime.setText(completeTime);
+//        if (workOrderInfo.getCompleteTime() == 0) {
+//            tvCompleteTime.setVisibility(View.GONE);
+//        }
+//        String confirm = "复核人员: " + workOrderInfo.getConfirm();
+//        tvConfirm.setText(confirm);
+//        String confirmTime = "复核时间: " + new DateTime(workOrderInfo.getConfirmTime()).toString("yyyy-MM-dd HH:mm:ss");
+//        tvConfirmTime.setText(confirmTime);
+//        if (workOrderInfo.getConfirm() == null) {
+//            tvConfirm.setVisibility(View.GONE);
+//            tvConfirmTime.setVisibility(View.GONE);
+//        }
+//
+//        //
+//        List<WorkOrderSupplyInfo> workOrderSupplyInfoList = workOrderInfo.getSupplyList();
+//        if (workOrderSupplyInfoList.size() == 0) {
+//            tvPack.setVisibility(View.GONE);
+//        } else {
+//            String pack = "领料情况: ";
+//            for (WorkOrderSupplyInfo workOrderSupplyInfo : workOrderSupplyInfoList) {
+//                pack += (workOrderSupplyInfo.getMaterial() + "(" + workOrderSupplyInfo.getAmount() + ") ");
+//            }
+//            tvPack.setText(pack);
+//        }
+//    }
+
+}
--- app/src/main/java/com/unicorn/csp/xcdemo/fragment/technician/TestFragment.java
+++ app/src/main/java/com/unicorn/csp/xcdemo/fragment/technician/TestFragment.java
@@ -1,14 +1,34 @@
 package com.unicorn.csp.xcdemo.fragment.technician;
 
 import com.unicorn.csp.xcdemo.R;
+import com.unicorn.csp.xcdemo.component.WorkOrderCard;
 import com.unicorn.csp.xcdemo.fragment.base.ButterKnifeFragment;
 
-/**
- * Created by Administrator on 2015/11/24.
- */
+import butterknife.Bind;
+import butterknife.OnClick;
+
+
 public class TestFragment extends ButterKnifeFragment {
+
     @Override
     public int getLayoutResId() {
-        return R.layout.tab;
+        return R.layout.fragment_test;
     }
+
+    @Bind(R.id.word_order_card)
+    WorkOrderCard workOrderCard;
+
+    boolean expand = false;
+    @OnClick(R.id.word_order_card)
+    public void func(){
+        if (expand) {
+            workOrderCard.collapse();
+        expand =false;
+        }else {
+            workOrderCard.expand();
+            expand =true;
+        }
+        }
+
+
 }
